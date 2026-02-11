2c051494198f00dce8a4b94f1e2f36de1382a9a1
hiram <pazbravoluis@gmai.com>
2026-02-10 17:02:49 -0800

    pythom basico

---
+++ sesion_01.py
@@ -0,0 +1,24 @@
+# Numeros 
+print(int(7))
+print(float(7.7))
+type(7)
+type(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print(int(1 + 4 - 2))
+print(float(1 + 2.0))
+
+# operadores matematicas
+# +
+# -
+# *
+# /
+# **
+# % modulo
+
+print(int( 2**3))
+print(int(4**8))
+print(float(10%3))
+print(int(25%4))
+print(float(16%2))
+print(float(10 / 3))
